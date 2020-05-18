5eaa9b855c4e81b8a5b0b057498f4fe8cfe11457
JiangNinShang <1753291617@qq.com>
2020-05-18 11:09:26 +0800

    Commodity

--- src/main/java/main/controller/CommodityController.java
+++ src/main/java/main/controller/CommodityController.java
@@ -3,20 +3,27 @@ package main.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
 import main.dao.CommodityMapper;
 import main.domin.Commodity;
+import main.service.CommodityService;
 
 @RequestMapping("Commodity")
 @RestController
 public class CommodityController {
 	@Autowired
-	CommodityMapper cm;
+	CommodityService cs;
 	
-//	@RequestMapping("getCommodity")
-//	public List<Commodity> getCommodity(){
-//		return list;
-//	}
+	@ApiOperation("删除")
+	@PostMapping("/deAddress")
+	@ResponseBody
+	@RequestMapping("getCommodity")
+	public List<Commodity> getCommodity(){
+		return cs.getCommodity();
+	}
 }
---
+++ src/main/java/main/service/CommodityService.java
@@ -0,0 +1,9 @@
+package main.service;
+
+import java.util.List;
+
+import main.domin.Commodity;
+
+public interface CommodityService {
+	List<Commodity> getCommodity();
+}
